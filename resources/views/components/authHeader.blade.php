da2db6df46a2324cc70f80f3ccc08784bc36b416
Jerwin <144522990+Neophyte-code@users.noreply.github.com>
2026-02-11 04:59:13 +0800

    add landing page

--- resources/views/components/authHeader.blade.php
+++ resources/views/components/authHeader.blade.php
@@ -1,31 +1,15 @@
-<header class="absolute inset-x-0 top-0 z-50">
-    <div class="navbar bg-base-100 shadow-sm">
-        <div class="navbar-start">
-            <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                </div>
-                <ul
-                    tabindex="-1"
-                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li><a>Home</a></li>
-                    <li><a>About</a></li>
-                    <li><a>Contact</a></li>
-                </ul>
+<div class="navbar bg-base-100 shadow-sm p-5">
+    <div class="navbar-start">
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
             </div>
-            <a class="btn btn-ghost text-xl">Waste<span class="text-green-600">Wise</span></a>
         </div>
-        <div class="navbar-center hidden lg:flex">
-            <ul class="menu menu-horizontal px-1">
-                <li class="hover:text-green-600 font-bold"><a>Home</a></li>
-                <li class="hover:text-green-600 font-bold"><a>About</a></li>
-                <li class="hover:text-green-600 font-bold"><a>Contact</a></li>
-            </ul>
-        </div>
-        <div class="navbar-end">
-            <a class="btn">Button</a>
-        </div>
+        <a href="" class="font-extrabold text-xl sm:text-2xl ">Waste<span class="text-green-600">Watch</span></a>
     </div>
-</header>
+    <div class="navbar-end">
+        <a href="" class="btn text-white bg-green-600 hover:bg-green-500">Login</a>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,37 @@
 <body>
   <!-- header -->
   <x-authHeader />
-  <main class="">
 
+  <main class="h-screen">
+    <div class="flex items-center justify-center gap-4 h-screen bg-cover p-10" style="background-image: url('/images/landing.jpg')">
+      <div class="h-full max-w-2xl flex justify-center flex-col mr-auto">
+        <div class="p-5 text-sm/7">
+          <h2 class="text-white font-bold text-5xl">Welcome to Waste<span class="text-green-600">Watch</span></h2>
+          <h3 class="text-4xl font-bold mt-2">Earn by reporting <span id="report" class="text-green-600"></span></h3>
+          <p class="text-xl mt-5">WasteWatch is a user-friendly system designed to help individuals and communities report waste effectively. Our platform enables users to easily log and track waste, providing valuable insights into waste management practices and identifying areas for improvement. By promoting transparency and accountability, WasteWise aims to contribute to a more sustainable future for our planet.</p>
+        </div>
+      </div>
+    </div>
   </main>
+
+
+  <!-- footer -->
+  <x-footer />
+  <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
+  <script>
+    var app = document.getElementById('report');
+    var typewriter = new Typewriter(app, {
+      loop: true,
+      delay: 150,
+    });
+    typewriter
+      .typeString('Waste')
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString('Litterer')
+      .pauseFor(2000)
+      .start();
+  </script>
 </body>
 
 </html>
