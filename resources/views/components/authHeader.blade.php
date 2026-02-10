7ed77bfd6243597c2954b4ef580b0f9775a03481
Jerwin <144522990+Neophyte-code@users.noreply.github.com>
2026-02-11 01:29:02 +0800

    Initial commit

---
+++ resources/views/components/authHeader.blade.php
@@ -0,0 +1,31 @@
+<header class="absolute inset-x-0 top-0 z-50">
+    <div class="navbar bg-base-100 shadow-sm">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </div>
+                <ul
+                    tabindex="-1"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </div>
+            <a class="btn btn-ghost text-xl">Waste<span class="text-green-600">Wise</span></a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+                <li class="hover:text-green-600 font-bold"><a>Home</a></li>
+                <li class="hover:text-green-600 font-bold"><a>About</a></li>
+                <li class="hover:text-green-600 font-bold"><a>Contact</a></li>
+            </ul>
+        </div>
+        <div class="navbar-end">
+            <a class="btn">Button</a>
+        </div>
+    </div>
+</header>
